888f60cc848cbfdd7bff90b71d828b2581d58f8a
Никита <killerlord1988@gmail.com>
2020-07-27 19:01:56 +0400

    fix There are no files, modules and parts of code that are not used in the project code, including commented-out code sections

--- js/util.js
+++ js/util.js
@@ -5,23 +5,5 @@
   window.util = {
     ENT_KEY: 'Enter', // Keycode of enter button
     ESC_KEY: 'Escape', // Keycode of escape button
-
-    // Generating a random array
-    getRandomData: function (dataSet) {
-
-      // Generating a random array length
-      var randomNumber = this.getRandomFloat(0, dataSet.length);
-      var data = [];
-      // Write data from the array passed to the parameter into a random array
-      for (var key = 0; key < randomNumber; key++) {
-        data.push(dataSet[key]);
-      }
-      return data;
-    },
-
-    // Create a function to generate a random number from min to max
-    getRandomFloat: function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
   };
 })();
